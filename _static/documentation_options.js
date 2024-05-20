f7dbdfb971f8245cdf360054ed1c4f53ad76de76
boehmseb <boehmseb@users.noreply.github.com>
2024-05-20 13:52:05 +0000

    deploy: 3fd7ca2502c0263126a49bcb725aebe61be44b26

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.0.2.dev993+g3fd7ca25',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'dirhtml',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
